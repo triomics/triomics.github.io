b8536474829dbb745d1f0b86819d1ad2c1385270
Roman <romeg33@ukr.net>
2016-08-16 19:06:52 +0300

    some minor changes to popup msg form logic

--- js/script.js
+++ js/script.js
@@ -245,8 +245,9 @@ var form = $(this);
                 complete: function(data) { 
                       form.find('input[type="submit"]').prop('disabled', false); 
                       $(".required").css('display','none');
-                      $(".msg-sent").html('Message sent').fadeIn(2000); 
-                      setTimeout(function(){$('.msg-sent').fadeOut();}, 2000);
+                      $(".msg-sent").html('Message sent').fadeIn(1000); 
+                      setTimeout(function(){$('.msg-sent').fadeOut();}, 3000);
+                      $('input').val('');
                   }
                             
                 });
